2a0fb5058d1533e64893091d590e9aedd596cae7
Tavera15 <joaquinramirez97@yahoo.com>
2024-11-14 01:16:39 -0600

    Uploaded images to projecs page

--- src/Components/PortfolioCard/PortfolioCard.jsx
+++ src/Components/PortfolioCard/PortfolioCard.jsx
@@ -1,12 +1,19 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { Button, Modal } from "react-bootstrap";
 
-function PortfolioCard({photo})
+function PortfolioCard({name, category, src})
 {
+    const projectsDir = "../../assets/Projects/"
+    const [photo, setPhoto] = useState("");
+
+    useEffect(() => {
+        setPhoto(new URL(`${projectsDir}${category}/${src}`, import.meta.url).href);
+    }, [])
+    
     const [modalShow, setModalShow] = useState(false);
 
     return(
-        <div className="card col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+        <div className="card col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-4">
             <Modal
                 show={modalShow}
                 onHide={setModalShow}
@@ -16,7 +23,7 @@ function PortfolioCard({photo})
                 >
                 <Modal.Header closeButton></Modal.Header>
                 <Modal.Body>
-                    <img className="col-12" src={photo} alt={"modal " + photo}/>
+                    <img className="col-12" src={photo} alt={"modal " + name}/>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={() => setModalShow(false)}>Close</Button>
@@ -24,9 +31,11 @@ function PortfolioCard({photo})
                 </Modal>
 
             <div onClick={() => setModalShow(true)}>
-                <img className="card-img-top" style={{"height": "15rem", "objectFit": "cover"}} src={photo} alt="Card image cap" />
-                <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                {
+                    <img className="card-img-top border" src={photo} style={{"height": "15rem", "objectFit": "cover"}} alt="Card image cap" />
+                }
+                <div className="card-body mt-2 mb-2">
+                    <h4 className="card-text">{name}</h4>
                 </div>
             </div>
         </div>
--- src/Pages/PortfolioPage/PortfolioPage.jsx
+++ src/Pages/PortfolioPage/PortfolioPage.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PortfolioPage.css";
 import PortfolioCard from "../../Components/PortfolioCard/PortfolioCard";
 import logo from "../../assets/logo.png";
+import data from "../../assets/Projects/projects.json";
 
 function PortfolioPage()
 {
+    const [projects, setProjects] = useState([]);
+    const [categories, setCategories] = useState(new Set());
+
+    useEffect(() => {
+        data.map((d, i) => {
+            setCategories(prev => new Set([...prev, d.category]))
+        })
+    }, [])
+
     return(
         <div className="d-flex m-0 row justify-content-start">
             <div className="col-12 d-flex justify-content-center banner">
@@ -13,37 +23,21 @@ function PortfolioPage()
 
             <div className="scrollmenu">
                 <a href="#home">Home</a>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-                <a href="#support">Support</a>
-                <a href="#blog">Blog</a>
-                <a href="#tools">Tools</a>  
-                <a href="#base">Base</a>
-                <a href="#custom">Custom</a>
-                <a href="#more">More</a>
-                <a href="#logo">Logo</a>
-                <a href="#friends">Friends</a>
-                <a href="#partners">Partners</a>
-                <a href="#people">People</a>
-                <a href="#work">Work</a>
+                {
+                    [...categories].map((c, i) => (<a href={c} key={i}>{c}</a>))
+                }
             </div>
 
             <div className="d-flex justify-content-center">
 
-                <div className="col-10 d-flex justify-content-start align-items-stretch row mt-4 mb-4">
-                    <PortfolioCard photo="https://marketplace.canva.com/EAFQHkE0bec/1/0/1600w/canva-red-black-creative-modern-business-card-LqUfJRUvvrI.jpg" />
-                    <PortfolioCard photo="https://marketplace.canva.com/EAFUj7uKPZI/1/0/1600w/canva-modern-creative-business-card-WtpSYS6bMZ4.jpg" />
-                    <PortfolioCard photo="https://templates.designwizard.com/75442f90-1550-11e8-a532-41cc5dc1baad.jpg" />
-                    <PortfolioCard photo="https://designshifu.com/wp-content/uploads/2022/11/Ad-banner-design-ideas-examples-that-get-the-maximum-clicks.jpg" />
-                    <PortfolioCard photo="https://www.bdtask.com/blog/assets/plugins/ckfinder/core/connector/php/uploads/images/restaurant-menu-design-1.jpg" />
-                    <PortfolioCard photo="https://insightsigncompany.com/wp-content/uploads/magnetic-sign-1.jpg" />
-                    <PortfolioCard photo="https://blog.photoadking.com/wp-content/uploads/2023/04/1682070770785.jpg" />
-                    <PortfolioCard photo="https://blog.photoadking.com/wp-content/uploads/2023/04/1682070770785.jpg" />
-                    <PortfolioCard photo="https://blog.photoadking.com/wp-content/uploads/2023/04/1682070770785.jpg" />
-                    <PortfolioCard photo="https://blog.photoadking.com/wp-content/uploads/2023/04/1682070770785.jpg" />
-                    <PortfolioCard photo="https://blog.photoadking.com/wp-content/uploads/2023/04/1682070770785.jpg" />
-                    <PortfolioCard photo="https://blog.photoadking.com/wp-content/uploads/2023/04/1682070770785.jpg" />
+                <div className=" container justify-content-start align-items-stretch row mt-4 mb-4">
+
+                    {
+                        data.map((d, i) => {
+                            return (<PortfolioCard name={d.name} category={d.category} src={d.src} key={i}/>)
+                        })
+                    }
+
                 </div>
             </div>
         </div>
